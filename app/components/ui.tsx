30520345a58d2e597420248cf153cb4bde9358ff
JOYBOY-0 <falamino00@gmail.com>
2025-05-31 17:12:05 -0300

    upd

--- app/components/ui.tsx
+++ app/components/ui.tsx
@@ -35,7 +35,8 @@ export const UI = () => {
 
   if (debug) return null;
 
-  const isLobby = isGameRoute && id && state?.status === "lobby" && playerId;
+  const isLobby =
+    isGameRoute && id && state?.status === "lobby" && playerId !== undefined;
 
   return (
     <div className="fixed inset-0 z-[9999] pointer-events-none flex items-center justify-center">
