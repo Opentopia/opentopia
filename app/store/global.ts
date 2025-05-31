b911a963d6f915430903e42d752cb3847f411dd4
Julian Benegas <julianbenegas99@gmail.com>
2025-05-31 15:20:27 -0300

    dude

--- app/store/global.ts
+++ app/store/global.ts
@@ -1,10 +1,10 @@
-import type { TileKey } from 'workers/mechanics';
-import { create } from 'zustand'
+import type { TileKey } from "workers/mechanics";
+import { create } from "zustand";
 
 export const useGlobalStore = create<{
   hoveredBlock: TileKey | null;
   hoverBlock: (block: TileKey | null) => void;
-}>((set) => ({
+}>(set => ({
   hoveredBlock: null,
   hoverBlock: (block: TileKey | null) => set({ hoveredBlock: block }),
-}))
+}));
