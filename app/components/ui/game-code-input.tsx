37e226112546eb2009260e177bb698e863f54a98
JOYBOY-0 <falamino00@gmail.com>
2025-05-31 18:42:03 -0300

    upd

--- app/components/ui/game-code-input.tsx
+++ app/components/ui/game-code-input.tsx
@@ -2,30 +2,34 @@ import * as React from "react";
 import { Input } from "./input";
 import { cn } from "@/lib/utils";
 import { Button } from "./button";
+import { useNavigate } from "react-router";
 
 interface GameCodeInputProps
   extends Omit<
     React.ComponentProps<"input">,
     "onChange" | "maxLength" | "placeholder"
   > {
-  onChange?: (value: string) => void;
   placeholder?: string;
 }
 
 function GameCodeInput({
   className,
-  onChange,
   placeholder = "ABC123",
   ...props
 }: GameCodeInputProps) {
+  const navigate = useNavigate();
+  const [value, setValue] = React.useState("");
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
       .toUpperCase()
       .replace(/[^A-Z0-9]/g, "")
       .slice(0, 6);
     e.target.value = value;
-    onChange?.(value);
+    setValue(value);
   };
+
+  const valueIsValid = value.length === 6 && /^[A-Z0-9]+$/.test(value);
 
   return (
     <div className="w-full h-12 md:h-[80px] min-h-12 md:min-h-[80px] max-h-12 md:max-h-[80px] flex overflow-hidden">
@@ -35,6 +39,7 @@ function GameCodeInput({
           "w-full !h-full min-h-0 max-h-12 md:max-h-[80px] md:px-4 md:py-3 text-2xl md:text-5xl text-center font-display font-bold uppercase overflow-hidden text-ellipsis rounded-r-none border-r-0 flex-shrink",
           className,
         )}
+        value={value}
         onChange={handleChange}
         maxLength={6}
         placeholder={placeholder}
@@ -42,6 +47,12 @@ function GameCodeInput({
       />
 
       <Button
+        disabled={!valueIsValid}
+        onClick={() => {
+          if (valueIsValid) {
+            navigate(`/games/${value}`);
+          }
+        }}
         variant="tertiary"
         className="h-12 md:h-[80px] min-h-12 md:min-h-[80px] max-h-12 md:max-h-[80px] aspect-square rounded-l-none flex-shrink-0"
       >
